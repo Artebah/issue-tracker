bf9016d1575d29fec032fba7dedab4db88b9ad9c
Artem Litvin <mafioznik445@gmail.com>
2024-10-12 12:10:37 +0300

    Setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@radix-ui/themes";
 import React from "react";
 
 const IssuesPage = () => {
-  return <div>Issues list</div>;
+  return (
+    <div>
+      <h1>Issues list</h1>
+      <Button>Click me!</Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
