eb70c9ecd0aa418e68b645e3d001c618acb392a1
Artem Litvin <mafioznik445@gmail.com>
2024-10-13 10:03:55 +0300

    Show issues list & add responsive layout

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,13 +1,39 @@
-import { Button } from "@radix-ui/themes";
+import prisma from "@/prisma/client";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
+
   return (
     <div>
-      <Button>
-        <Link href="/issues/new">Create a new issue</Link>
-      </Button>
+      <div className="mb-5">
+        <Button>
+          <Link href="/issues/new">Create a new issue</Link>
+        </Button>
+      </div>
+      <Table.Root>
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:block">Status</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:block">Created</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map(({ id, title, description, status, updatedAt, createdAt }) => (
+            <Table.Row key={id}>
+              <Table.Cell>
+                <p>{title}</p>
+                <p className="block md-hidden text-xs mt-1">{status}</p>
+              </Table.Cell>
+              <Table.Cell className="hidden md:block">{status}</Table.Cell>
+              <Table.Cell className="hidden md:block">{createdAt.toLocaleDateString()}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 };
