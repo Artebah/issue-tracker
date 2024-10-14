9053c711b42d632300dd3d528ffc2c8a407200f5
Artem Litvin <mafioznik445@gmail.com>
2024-10-14 17:37:26 +0300

    Populate the assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,14 +1,28 @@
+"use client";
+
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import axios from "axios";
 import React from "react";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = React.useState<User[]>([]);
+
+  React.useEffect(() => {
+    axios.get<User[]>("/api/users").then(({ data }) => setUsers(data));
+  }, []);
+
   return (
     <Select.Root defaultValue="apple">
       <Select.Trigger>Assign to</Select.Trigger>
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Artem Litvin</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
