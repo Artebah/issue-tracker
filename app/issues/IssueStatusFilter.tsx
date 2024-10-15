9d4ccfc2a6a42a310cf3bf93e867c5c327f9657d
Artem Litvin <mafioznik445@gmail.com>
2024-10-15 11:10:41 +0300

    Add issue status filter

---
+++ app/issues/IssueStatusFilter.tsx
@@ -0,0 +1,31 @@
+import { Status } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import React from "react";
+
+interface IStatusObj {
+  label: string;
+  value?: Status;
+}
+const statuses: IStatusObj[] = [
+  { label: "All" },
+  { label: "Open", value: "OPEN" },
+  { label: "Closed", value: "CLOSED" },
+  { label: "In progress", value: "IN_PROGRESS" },
+];
+
+const IssueStatusFilter = () => {
+  return (
+    <Select.Root>
+      <Select.Trigger placeholder="Filter by status..." />
+      <Select.Content>
+        {statuses.map((status) => (
+          <Select.Item key={status.label} value={status.value || " "}>
+            {status.label}
+          </Select.Item>
+        ))}
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default IssueStatusFilter;
--- app/issues/IssuesToolbar.tsx
+++ app/issues/IssuesToolbar.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
+import IssueStatusFilter from "./IssueStatusFilter";
 
 const IssuesToolbar = () => {
   return (
-    <div className="mb-5">
+    <div className="mb-5 flex justify-between">
+      <IssueStatusFilter />
       <Button>
         <Link href="/issues/new">Create a new issue</Link>
       </Button>
