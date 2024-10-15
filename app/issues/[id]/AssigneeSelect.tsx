f09c5dfd7130cef8f00ed0611a1b827c64a0898e
Artem Litvin <mafioznik445@gmail.com>
2024-10-15 09:01:40 +0300

    Fetch data with React Query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,16 +1,28 @@
 "use client";
 
 import { User } from "@prisma/client";
-import { Select } from "@radix-ui/themes";
+import { Select, Skeleton } from "@radix-ui/themes";
 import axios from "axios";
 import React from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = React.useState<User[]>([]);
+  const {
+    data: users = [],
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    // identifier for caching
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then(({ data }) => data),
+    // refresh every 60s
+    staleTime: 60 * 1000,
+    // count of retries before stopping sending requests
+    retry: 3,
+  });
 
-  React.useEffect(() => {
-    axios.get<User[]>("/api/users").then(({ data }) => setUsers(data));
-  }, []);
+  if (isLoading) return <Skeleton height="2rem" width=" 6.25rem" />;
+  if (error) return null;
 
   return (
     <Select.Root defaultValue="apple">
