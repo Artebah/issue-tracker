0b0ce39f89aa3577ba59c59aaf78ba9d48429b85
Artem Litvin <mafioznik445@gmail.com>
2024-10-15 14:24:50 +0300

    Implement pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { Button } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 import {
   MdOutlineKeyboardDoubleArrowRight,
@@ -14,26 +16,58 @@ interface Props {
 }
 
 const Pagination = ({ currentPage, itemCount, pageSize }: Props) => {
-  const pageCount = Math.ceil(itemCount / pageSize);
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
+  const pageCount = Math.ceil(itemCount / pageSize);
   if (pageCount <= 1) return null;
+
+  const changePage = (page: number) => {
+    let newPageCount = page;
+    if (page > pageSize) {
+      newPageCount = pageSize;
+    }
+    if (page < 1) {
+      newPageCount = 1;
+    }
+
+    const params = new URLSearchParams(searchParams);
+    params.set("page", newPageCount.toString());
+    router.push(`?${params.toString()}`);
+  };
 
   return (
     <div className="flex items-center gap-2">
       <h3>
         Page {currentPage} of {pageCount}{" "}
       </h3>
-      <Button variant="soft" color="gray" disabled={currentPage === 1}>
+      <Button
+        variant="soft"
+        color="gray"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 10)}>
         <MdOutlineKeyboardDoubleArrowLeft size={25} />
       </Button>
-      <Button variant="soft" color="gray" disabled={currentPage === 1}>
+      <Button
+        variant="soft"
+        color="gray"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}>
         <MdChevronLeft size={25} />
       </Button>
-      <Button variant="soft" color="gray" disabled={currentPage === pageCount}>
-        <MdOutlineKeyboardDoubleArrowRight size={25} />
-      </Button>
-      <Button variant="soft" color="gray" disabled={currentPage === pageCount}>
+      <Button
+        variant="soft"
+        color="gray"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}>
         <MdChevronRight size={25} />
+      </Button>
+      <Button
+        variant="soft"
+        color="gray"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 10)}>
+        <MdOutlineKeyboardDoubleArrowRight size={25} />
       </Button>
     </div>
   );
